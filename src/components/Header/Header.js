d49a28224b42207344f989a0ca5217507e8600e1
RonnyPumaricra <ronnylpqwerty@gmail.com>
2023-03-17 17:30:41 -0500

    Exercise 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import UnstyledButton from '../UnstyledButton';
+import Icon from '../Icon';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -30,6 +32,26 @@ const Header = () => {
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
         <Side />
+        <MobileNav>
+          <UnstyledButton>
+            <Icon
+              size={24}
+              id={"shopping-bag"}
+            />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon
+              size={24}
+              id={"search"}
+            />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon
+              size={24}
+              id={"menu"}
+            />
+          </UnstyledButton>
+        </MobileNav>
       </MainHeader>
 
       <MobileMenu
@@ -46,12 +68,19 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.phoneAndDown} {
+    padding: 18px 16px;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const Side = styled.div`
@@ -69,5 +98,17 @@ const NavLink = styled.a`
     color: ${COLORS.secondary};
   }
 `;
+
+/* Responsive Components */
+const MobileNav = styled.div`
+  display: flex;
+  gap: 34px;
+  @media ${QUERIES.phoneAndDown} {
+    gap: 16px;
+  }
+  @media not ${QUERIES.tabletAndDown} {
+    display: none;
+  }
+`
 
 export default Header;
